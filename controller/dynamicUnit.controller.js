cc2981433d2182dd37dd5d68cf628a4d56cacb56
Praveen Patidar <pravinpatidar718@gmail.com>
2026-05-15 11:53:23 +0530

    upd

--- controller/dynamicUnit.controller.js
+++ controller/dynamicUnit.controller.js
@@ -24,8 +24,8 @@ export const saveDynamicUnit = async (req, res, next) => {
 
 export const viewDynamicUnit = async (req, res, next) => {
     try {
-        const dynamicUnit = await DynamicUnit.findOne({ database: req.params.database })
-        return dynamicUnit? res.status(200).json({ message: "Data Found", dynamicUnit, status: true }) : res.status(404).json({ message: "Not Found", status: false })
+        const dynamicUnit = await DynamicUnit.find({ database: req.params.database })
+        return dynamicUnit.length>0? res.status(200).json({ message: "Data Found", dynamicUnit, status: true }) : res.status(404).json({ message: "Not Found", status: false })
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: "Internal Server Error", error: error.message, status: false })
