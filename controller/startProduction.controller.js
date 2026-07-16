c98feef3f6934e651b4eb105d9370c1b1443495b
Praveen Patidar <pravinpatidar718@gmail.com>
2026-07-16 12:55:03 +0530

    u

--- controller/startProduction.controller.js
+++ controller/startProduction.controller.js
@@ -1007,7 +1007,7 @@ export const wastageProductReport = async (req, res, next) => {
           wastageProducts.push({
             date: production.date,
             productName: wastage.wProduct_name?.productName,
-            productSection: wastage.wProduct_name?.producttype,
+            productSection: wastage.wProduct_name?.step_Name,
             units: wastage.wProduct_name_Units,
           });
         });
