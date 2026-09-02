eec1c4ca4e474efd5c3b64b131305a9c3ca983f8
Praveen Patidar <pravinpatidar718@gmail.com>
2026-09-02 11:34:36 +0530

    upd

--- model/ProductionTarget.model.js
+++ model/ProductionTarget.model.js
@@ -1,51 +1,117 @@
 import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema(
+const InputProductDetailSchema = new mongoose.Schema(
   {
-    productId: {
+    unit: {
       type: String,
       required: true,
     },
-    productName: {
+    qty: {
+      type: Number,
+      required: true,
+    },
+    source: {
       type: String,
+      required: true,
+    },
+    operator: {
+      type: String,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
+const OutputProductDetailSchema = new mongoose.Schema(
+  {
+    unit: {
+      type: String,
+      required: true,
+    },
+    inputQty: {
+      type: Number,
       required: true,
     },
     targetQty: {
       type: Number,
       required: true,
     },
-    productDetails: {
-      unit: {
-        type: String,
-      },
-      value: {
-        type: Number,
-      },
-     },
+    operator: {
+      type: String,
+      required: true,
+    },
+    calculatedQty: {
+      type: Number,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
+const ProductSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: String,
+      required: true,
+    },
+
+    productName: {
+      type: String,
+      required: true,
+    },
+
+    targetQty: {
+      type: Number,
+      required: true,
+    },
+
+    inputProductDetails: {
+      type: [InputProductDetailSchema],
+      default: [],
+    },
+
+    outputProductDetails: {
+      type: [OutputProductDetailSchema],
+      default: [],
+    },
   },
   { _id: false }
 );
 
 const productionTargetSchema = new mongoose.Schema(
   {
+    created_by: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
     database: {
       type: String,
       required: true,
     },
+
     workerId: {
       type: String,
     },
+
     financialYear: {
       type: String,
       required: true,
     },
+
     month: {
       type: String,
     },
+
+    reportType: {
+      type: String,
+    },
+
     products: {
       type: [ProductSchema],
       default: [],
     },
+
     status: {
       type: String,
       default: "Active",
