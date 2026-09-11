d725bf57f51e45e2a80a4ddc4cb8300502d51b45
Praveen Patidar <pravinpatidar718@gmail.com>
2026-09-11 12:57:41 +0530

    upd

--- routes/startProduction.route.js
+++ routes/startProduction.route.js
@@ -30,6 +30,6 @@ router.get("/worker-report/:financeYear/:workerId",workerReport)
 router.post("/save-worker-target",productionTarget)
 router.get("/view-workerTarget/:database/:financeYear",listOfProductionTarget)
 router.delete("/delete-workerTaget/:id",deleteProductionTarget)
-router.update("/update-workerTaget/:id",updateProductionTarget)
+router.put("/update-workerTaget/:id",updateProductionTarget)
 
 export default router;
